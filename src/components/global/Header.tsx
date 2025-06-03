43643bfd0b007c24933a2d886d163204ac8b0799
Digital-Alchemyst <digitalalchemiststudios@gmail.com>
2025-06-03 16:22:47 -0500

    Header

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,55 +1,49 @@
-import React from "react";
-import { Github } from "lucide-react";
-import { ThemeToggle } from "@/components/global/ThemeToggle";
-import Link from "next/link";
+import React from 'react';
+import { Github } from 'lucide-react';
+import { ThemeToggle } from '@/components/global/ThemeToggle';
+import Link from 'next/link';
 
-
-import { Button } from "../ui/button";
-import Image from "next/image";
-import Search from "./Search";
-import { TrolleyIcon } from "@sanity/icons";
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import Search from './Search';
+import { TrolleyIcon } from '@sanity/icons';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full">
-      <div className="border-b border-light-300 bg-light-100/75 backdrop-blur-lg dark:border-dark-600 dark:bg-dark-800/75">
-        <div className="container flex h-16 items-center justify-between px-4">
-          <div className="flex items-center space-x-2">
-            <h1 className="relative">
+    <header className='sticky top-0 z-50 w-full'>
+      <div className='border-b border-light-300 bg-light-100/75 backdrop-blur-lg dark:border-dark-600 dark:bg-dark-800/75'>
+        <div className='container flex h-16 items-center justify-between px-4'>
+          <div className='flex items-center space-x-2'>
+            <h1 className='relative'>
               <Button
-                variant="default"
-                size="lg"
-                className="text-transparent text-gradient-orange-purple"
+                variant='default'
+                size='lg'
+                className='text-transparent text-gradient-orange-purple'
               >
                 <Image
-                  src="/logo.jpg"
-                  alt=""
+                  src='/logo.jpg'
+                  alt=''
                   width={50}
                   height={50}
-                  className="h-8 w-8 rounded-full"
+                  className='h-8 w-8 rounded-full'
                 />
-                <h3 className="hidden sm:block">Alchemy Collectibles</h3>
+                <h3 className='hidden sm:block'>Next Alchemy 15</h3>
               </Button>
             </h1>
           </div>
-          <div className="hidden sm:block flex-1 mx-4">
+          <div className='mx-4 hidden flex-1 sm:block'>
             <Search />
           </div>
-          <div className="flex items-center space-x-2 sm:space-x-6">
-            <Link
-              className="flex items-center p-1 bg-dark-900 shadow hover:bg-dark-900/90 dark:bg-light-500 dark:hover:bg-light-500/90 border border-dark-900/10 dark:border-light-300/40 text-light-500 dark:text-dark-400 rounded-md"
-              href="/cart"
-            >
-              <TrolleyIcon className="h-5 w-5 sm:h-6 sm:w-6" />
-            </Link>
+          <div className='flex items-center space-x-2 sm:space-x-6'>
             <ThemeToggle />
           </div>
         </div>
-        <div className="sm:hidden border-t border-light-300 dark:border-dark-600 p-2">
+        <div className='border-t border-light-300 p-2 dark:border-dark-600 sm:hidden'>
           <Search />
         </div>
       </div>
-    </header>  );
+    </header>
+  );
 };
 
 export default Header;
